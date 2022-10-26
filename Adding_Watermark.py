79a1e566f85920428bfbecd140f58acdd4267f59
zarghammughal1 <55196035+zarghammughal1@users.noreply.github.com>
2022-10-26 22:22:36 +0500

    Convert code into function

--- Adding_Watermark.py
+++ Adding_Watermark.py
@@ -2,14 +2,20 @@ import PyPDF2
 import sys
 
 
+def add_watermark(original, watermark, output):
+    for i in range(original.getNumPages()):
+        page = original.getPage(i)
+        page.mergePage(watermark.getPage(0))
+        output.addPage(page)
+
+    return output
+
+
 original = PyPDF2.PdfFileReader(sys.argv[1])
 watermark = PyPDF2.PdfFileReader(sys.argv[2])
 output = PyPDF2.PdfFileWriter()
 
-for i in range(original.getNumPages()):
-    page = original.getPage(i)
-    page.mergePage(watermark.getPage(0))
-    output.addPage(page)
+result = add_watermark(original, watermark, output)
 
 with open('Watermarked_PDF.pdf', 'wb') as file:
-    output.write(file)
+    result.write(file)
